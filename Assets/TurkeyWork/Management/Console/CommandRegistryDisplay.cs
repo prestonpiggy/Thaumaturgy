b7167774b5211bdb5ad6503bcb7d9bde2f088195
SunriseHirame <bernhard.miettinen@gmail.com>
2017-11-25 01:48:37 +0200

    sad

---
+++ Assets/TurkeyWork/Management/Console/CommandRegistryDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TurkeyWork.Management {
+
+    public sealed class CommandRegistryDisplay : MonoBehaviour {
+
+        private CommandInterpreter interpreter;
+        public CommandBase[] Commands { get; private set; }
+
+        private void OnEnable () {
+            gameObject.hideFlags = HideFlags.DontSaveInBuild;
+        }
+
+        void Awake () {
+            interpreter = GetComponent<CommandInterpreter> ();
+            if (!interpreter) {
+                Debug.LogError ("Command Registry Display needs to be attached to the same GameObject with CommandInterpreter!");
+                return;
+            }
+        }
+
+        private void Start () {
+            GetCommands ();
+            interpreter.OnCommandsUpated += OnCommandsUpdated;
+        }
+
+        private void OnCommandsUpdated () {
+            GetCommands ();
+        }
+
+        private void GetCommands () {
+            Commands = interpreter?.GetRegisteredCommands ();
+        }
+    }
+}
