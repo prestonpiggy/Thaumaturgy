ec890ba9a36b6cd5f5851108f23779879d85a642
Bernhard Miettinen <jesse.miettinen92@gmail.com>
2017-11-26 14:14:00 +0200

    asd

--- Assets/Thaumaturgy/Scripts/MainCamera.cs
+++ Assets/Thaumaturgy/Scripts/MainCamera.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainCamera : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        SceneManager.sceneLoaded += OnSceneLoaded;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void OnSceneLoaded (Scene scene, LoadSceneMode mode) {
+        var cameras = FindObjectsOfType<Camera> ();
+        foreach (var cam in cameras)
+            if (cam.Equals (this))
+                cam.gameObject.SetActive (false);
+    }
+
+    private void OnDestroy () {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
 }
