43fdf91e4ccbdf4d7e0237101839eb23fb2ca0d3
Bernhard Miettinen <jesse.miettinen92@gmail.com>
2017-11-30 18:31:25 +0200

    FIXED FUCKING EVERYTHING!
    NOW DRIKS, ALL AROUND!

---
+++ Assets/TurkeyWork/Management/LoadingScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TurkeyWork.Management {
+
+    public class LoadingScreen : MonoBehaviour {
+
+        [SerializeField] Text targetLevelText;
+
+        public string TargetLevelName {
+            get {
+                return targetLevelText.text;
+            }
+            set {
+                targetLevelText.text = value;
+            }
+        }
+    }
+
+
+}
